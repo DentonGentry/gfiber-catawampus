c74209d9c691022b5240ad2c5ef764a4fe252d4b
Denton Gentry <dgentry@google.com>
2011-07-11 11:20:18 -0700

    Add unmodified cpp-netlib-0.9.0.

---
+++ acs_connection.h
@@ -0,0 +1,22 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+// Author: dgentry@google.com (Denny Gentry)
+
+#ifndef ACS_CONNECTION_H_
+#define ACS_CONNECTION_H_
+
+class AcsConfig {
+ public:
+  AcsConfig() {}
+  ~AcsConfig() {}
+
+ private:
+};
+
+class AcsSession() {
+ public:
+  AcsSession() {}
+  ~AcsSession() {}
+ private:
+};
+
+#endif  // ACS_CONNECTION_H_
