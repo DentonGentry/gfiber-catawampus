6f1958985f06e7f6e270fcc384964a1240ef0337
Denton Gentry <dgentry@google.com>
2011-07-21 14:10:31 -0700

    Process status.

--- device_info.py
+++ device_info.py
@@ -8,11 +8,18 @@
 
 __author__ = 'dgentry@google.com (Denny Gentry)'
 
+import collections
+import glob
+import os
 import xmlwitch
 
+# Used for Device.DeviceStatus.ProcessStatus.Process.{i}
+Process = collections.namedtuple('Process', ['PID', 'Command', 'Size',
+                                             'Priority', 'CPUTime', 'State'])
+
 class DeviceInfoUno(object):
-  def __init__(self, uptime, memory_info):
-    self._manufacturer = "Google";
+  def __init__(self, uptime, memory_info, process_status):
+    self._manufacturer = "Google"
     self._manufacturer_oui = "00:1a:11:00:00:00"
     self._model_name = "Uno"
     self._description = "CPE device for Google Fiber network"
@@ -22,13 +29,26 @@ class DeviceInfoUno(object):
 
     self._uptime = uptime
     self._memory_info = memory_info
+    self._process_status = process_status
 
-  def MemInfoToXml(self, xml):
+  def _MemoryInfoToXml(self, xml):
     (totalmem, freemem) = self._memory_info.GetMemInfo()
     with xml.MemoryStatus:
       xml.Total(totalmem)
       xml.Free(freemem)
     return xml
+
+  def _ProcessStatusToXml(self, xml):
+    processes = self._process_status.GetProcesses()
+    with xml.Process:
+      for (num, proc) in enumerate(processes):
+        with xml.__getitem__(str(num)):
+          xml.PID(proc.PID)
+          xml.Command(proc.Command)
+          xml.Size(proc.Size)
+          xml.Priority(proc.Priority)
+          xml.CPUTime(proc.CPUTime)
+          xml.State(proc.State)
 
   def ToXml(self, xml):
     with xml.DeviceInfo:
@@ -40,7 +60,8 @@ class DeviceInfoUno(object):
       xml.HardwareVersion(self._hardware_version)
       xml.SoftwareVersion(self._software_version)
       xml.UpTime(self._uptime.GetUptime())
-      self.MemInfoToXml(xml)
+      self._MemoryInfoToXml(xml)
+      self._ProcessStatusToXml(xml)
     return xml
 
 class UptimeLinux26(object):
@@ -52,7 +73,7 @@ class UptimeLinux26(object):
     """
     try:
       uptime = float(open(self._proc_uptime).read().split()[0])
-    except:
+    except IOError, KeyError:
       # TODO(Dgentry) - LOG the exception, but return zeros by default
       uptime = 0.0
     return str(int(uptime))
@@ -64,26 +85,86 @@ class MemoryInfoLinux26(object):
 
   def GetMemInfo(self):
     """Returns two strings (TotalMem, FreeMem)"""
-    totalmem = 0
-    freemem = 0
+    totalmem = '0'
+    freemem = '0'
     try:
       pfile = open(self._proc_meminfo)
       for line in pfile:
-        name, value, unit = line.split()
+        fields = line.split()
+        name = fields[0]
+        value = fields[1]
         if name == "MemTotal:":
-          totalmem = int(value);
+          totalmem = value
         elif name == "MemFree:":
-          freemem = int(value);
-    except:
+          freemem = value
+    except IOError, KeyError:
       # TODO(Dgentry) - LOG the exception, but return zeros by default
       pass
-    return (str(totalmem), str(freemem))
+    return (totalmem, freemem)
+
+
+class ProcessStatusLinux26(object):
+  # Field ordering in /proc/<pid>/stat
+  _PID = 0
+  _COMM = 1
+  _STATE = 2
+  _UTIME = 13
+  _STIME = 14
+  _PRIO = 17
+  _RSS = 23
+
+  def __init__(self):
+    tick = os.sysconf(os.sysconf_names['SC_CLK_TCK'])
+    self._msec_per_jiffy = 1000 / tick
+    self._slash_proc = "/proc"
+
+  def _LinuxStateToTr181(self, linux_state):
+    mapping = {
+        "R" : "Running",
+        "S" : "Sleeping",
+        "D" : "Uninterruptible",
+        "Z" : "Zombie",
+        "T" : "Stopped",
+        "W" : "Uninterruptible"}
+    return mapping.get(linux_state, "Running")
+
+  def _JiffiesToMsec(self, utime, stime):
+    ticks = int(utime) + int(stime)
+    msecs = ticks * self._msec_per_jiffy
+    return str(msecs)
+
+  def _RemoveParens(self, command):
+    return command[1:-1]
+
+  def GetProcesses(self):
+    processes = list()
+    for proc in glob.glob(self._slash_proc + "/[0123456789]*/stat"):
+      try:
+         fields = open(proc).read().split()
+         p = Process(PID=fields[self._PID],
+                     Command=self._RemoveParens(fields[self._COMM]),
+                     Size=fields[self._RSS],
+                     Priority=fields[self._PRIO],
+                     CPUTime=self._JiffiesToMsec(fields[self._UTIME],
+                                                 fields[self._STIME]),
+                     State=self._LinuxStateToTr181(fields[self._STATE]))
+         processes.append(p)
+      except IOError:
+        # This isn't an error. We have a list of files which existed the
+        # moment the glog.glob was run. It a process exits before we get
+        # around to reading it, its /proc file will go away.
+        continue
+      except KeyError:
+        # TODO(dgentry) should LOG if exception is not IOError
+        continue
+    return processes
 
 
 def main():
   ut = UptimeLinux26()
   mi = MemoryInfoLinux26()
-  di = DeviceInfoUno(ut, mi)
+  ps = ProcessStatusLinux26()
+  di = DeviceInfoUno(ut, mi, ps)
   xml = xmlwitch.Builder(encoding='utf-8')
   print di.ToXml(xml)
 
--- device_info_test.py
+++ device_info_test.py
@@ -51,10 +51,48 @@ class DeviceInfoTest(unittest.TestCase):
     expected = ('0', '654321')
     self.assertEqual(mi.GetMemInfo(), expected)
 
+  def testProcessStatusReal(self):
+    ps = device_info.ProcessStatusLinux26();
+    # This fetches the processes running on the unit test machine. We can't
+    # make many assertions about this, just that there should be some processes
+    # running.
+    processes = ps.GetProcesses()
+    assert len(processes) > 0
+
+  def testProcessStatusFakeData(self):
+    fake_processes = {
+        "1"   : device_info.Process(PID="1", Command="init", Size="551",
+                                    Priority="20", CPUTime="81970",
+                                    State = "Sleeping"),
+        "3"   : device_info.Process(PID="3", Command="migration/0", Size="0",
+                                    Priority="-100", CPUTime="591510",
+                                    State = "Stopped"),
+        "5"   : device_info.Process(PID="5", Command="foobar", Size="0",
+                                    Priority="-100", CPUTime="591510",
+                                    State = "Zombie"),
+        "17"  : device_info.Process(PID="17", Command="bar", Size="0",
+                                    Priority="-100", CPUTime="591510",
+                                    State = "Uninterruptible"),
+        "164" : device_info.Process(PID="164", Command="udevd", Size="288",
+                                    Priority="16", CPUTime="300",
+                                    State = "Running"),
+        "770" : device_info.Process(PID="770", Command="automount", Size="6081",
+                                    Priority="20", CPUTime="5515790",
+                                    State = "Uninterruptible")
+        }
+    ps = device_info.ProcessStatusLinux26();
+    ps._slash_proc = "testdata/device_info/processes"
+    processes = ps.GetProcesses()
+    self.assertEqual(len(processes), 6)
+    for p in processes:
+      fake_p = fake_processes[p.PID];
+      self.assertEqual(fake_p, p)
+
   def testDeviceInfo(self):
     ut = UptimeMock()
     mi = MemoryInfoMock()
-    di = device_info.DeviceInfoUno(ut, mi)
+    ps = ProcessStatusMock()
+    di = device_info.DeviceInfoUno(ut, mi, ps)
     expected = """<DeviceInfo>
   <Manufacturer>Google</Manufacturer>
   <ManufacturerOUI>00:1a:11:00:00:00</ManufacturerOUI>
@@ -68,6 +106,24 @@ class DeviceInfoTest(unittest.TestCase):
     <Total>10</Total>
     <Free>20</Free>
   </MemoryStatus>
+  <Process>
+    <0>
+      <PID>1000</PID>
+      <Command>foo</Command>
+      <Size>100</Size>
+      <Priority>1</Priority>
+      <CPUTime>111111</CPUTime>
+      <State>Sleeping</State>
+    </0>
+    <1>
+      <PID>2000</PID>
+      <Command>bar</Command>
+      <Size>200</Size>
+      <Priority>2</Priority>
+      <CPUTime>222222</CPUTime>
+      <State>Running</State>
+    </1>
+  </Process>
 </DeviceInfo>"""
     xml = xmlwitch.Builder(encoding='utf-8')
     di.ToXml(xml)
@@ -81,6 +137,15 @@ class MemoryInfoMock(object):
 class UptimeMock(object):
   def GetUptime(self):
     return '888'
+
+class ProcessStatusMock(object):
+  def GetProcesses(self):
+    processes = list()
+    p = device_info.Process("1000", "foo", "100", "1", "111111", "Sleeping")
+    processes.append(p)
+    p = device_info.Process("2000", "bar", "200", "2", "222222", "Running")
+    processes.append(p)
+    return processes
 
 if __name__ == '__main__':
   unittest.main()
