7247451be9cf20093584e519417cd3f2a0785480
Avery Pennarun <apenwarr@gmail.com>
2011-08-25 05:52:20 -0700

    tr-schema.py: beginnings of a tr-xxx-*.xml data model parser.
    Current version handles <import> and toplevel <component> and <model>
    entries, and manages to resolve import references taken from other model files.
    Doesn't actually generate output yet.

---
+++ tr-schema.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+# Copyright 2011 Google Inc. All Rights Reserved.
+#
+"""Parser for tr-069-style data model .xml files."""
+
+__author__ = 'apenwarr@google.com (Avery Pennarun)'
+
+
+import xml.etree.ElementTree
+import re
+import sys
+
+
+chunks = {}
+imports = {}
+
+
+def Log(s):
+    sys.stderr.write('%s\n' % s)
+
+
+def AddChunk(spec, objtype, name, root):
+    key = (spec, objtype, name)
+    assert(not chunks.has_key(key))
+    chunks[key] = root
+
+
+def FixSpec(spec):
+    # When a spec refers to tr-xxx-1-0-0 or tr-xxx-1-0, we might have to
+    # substitute in tr-xxx-1-0-1 instead (a bugfix revision).  Let's just
+    # drop out the third version digit so it's easier to use as a dictionary
+    # key.
+    return re.sub(r'(:tr-\d+-\d+-\d+)-\d+$', r'\1', spec)
+
+
+def NiceSpec(spec):
+    return re.sub('^urn:broadband-forum-org:', '', spec)
+
+
+_ImportBugFixes = {
+    # bugs in tr-181-2-0-1.  It tries to import *_Device2, which doesn't
+    # seem to exist anywhere.
+    ('urn:broadband-forum-org:tr-143-1-0', 'component',
+       'DownloadDiagnostics_Device2'):
+      ('urn:broadband-forum-org:tr-143-1-0', 'component',
+       'DownloadDiagnostics'),
+    ('urn:broadband-forum-org:tr-143-1-0', 'component',
+       'UploadDiagnostics_Device2'):
+      ('urn:broadband-forum-org:tr-143-1-0', 'component', 
+       'UploadDiagnostics'),
+}
+
+
+def ParseImports(into_spec, root):
+    from_spec = FixSpec(root.attrib['spec'])
+    for node in root:
+        if node.tag in ('component', 'model'):
+            from_name = node.attrib.get('ref', node.attrib['name'])
+            into_name = node.attrib['name']
+            from_key = (from_spec, node.tag, from_name)
+            into_key = (into_spec, node.tag, into_name)
+            if from_key in _ImportBugFixes:
+                from_key = _ImportBugFixes[from_key]
+            assert(not chunks.has_key(into_key))
+            assert(not imports.has_key(into_key))
+            imports[into_key] = from_key
+        elif node.tag in ('dataType'):
+            continue
+        else:
+            raise KeyError(node.tag)
+
+
+def ResolveImports():
+    for k,v in sorted(imports.items()):
+        prefix = ' %-12s %-9s %-20s ' % (NiceSpec(k[0]), k[1], k[2])
+        Log("%s\n=%-12s %-9s %s" % (prefix, NiceSpec(v[0]), v[1], v[2]))
+        while imports.has_key(v):
+            v = imports[v]
+            Log("=%-12s %-9s %s" % (NiceSpec(v[0]), v[1], v[2]))
+        (into_spec, objtype, into_name) = k
+        (from_spec, objtype, from_name) = v
+        if objtype in ('component', 'model'):
+            AddChunk(into_spec, objtype, into_name,
+                     chunks[(from_spec, objtype, from_name)])
+        else:
+            raise KeyError(objtype)
+
+
+def main():
+    for filename in sys.argv[1:]:
+        root = xml.etree.ElementTree.parse(open(filename)).getroot()
+        spec = FixSpec(root.attrib['spec'])
+        Log(NiceSpec(spec))
+        for node in root:
+            if node.tag == 'import':
+                ParseImports(spec, node)
+            elif node.tag in ('component', 'model'):
+                name = node.attrib['name']
+                Log('%-12s %-9s %s' % (NiceSpec(spec), node.tag, name))
+                AddChunk(spec, node.tag, name, node)
+            elif node.tag in ('description', 'dataType', 'bibliography'):
+                continue
+            else:
+                Log('skip %s' % node.tag)
+    ResolveImports()
+
+
+if __name__ == "__main__":
+    main()
