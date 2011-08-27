5dbbef071e6058d2716743ce3cdbde0bdb901137
Avery Pennarun <apenwarr@gmail.com>
2011-08-26 22:05:47 -0700

    parse-schema: Now generates almost-valid python classes.
    Not *useful* ones, but the file looks pretty and understandable, at least.

--- tr069/parse-schema.py
+++ tr069/parse-schema.py
@@ -30,7 +30,7 @@ def FixSpec(spec):
     # substitute in tr-xxx-1-0-1 instead (a bugfix revision).  Let's just
     # drop out the third version digit so it's easier to use as a dictionary
     # key.
-    return re.sub(r'(:tr-\d+-\d+-\d+)-\d+$', r'\1', spec)
+    return re.sub(r':(tr|wt)-(\d+-\d+-\d+)-\d+$', r':tr-\2', spec)
 
 
 def NiceSpec(spec):
@@ -43,6 +43,10 @@ def SpecNameForPython(spec):
 
 def ObjNameForPython(name):
     return re.sub(r':(\d+)\.(\d+)', r'_v\1_\2', name)
+
+
+def Indented(prefix, s):
+    return re.sub(re.compile(r'^', re.M), prefix, unicode(s))
 
 
 _ImportBugFixes = {
@@ -111,37 +115,89 @@ def ResolveImports():
         else:
             raise KeyError(objtype)
 
+models = {}
 
-def RenderParameter(prefix, xml):
+class Model(object):
+    def __init__(self, spec, name, parent):
+        self.spec = SpecNameForPython(spec)
+        self.name = ObjNameForPython(name)
+        if parent:
+            self.parent = ObjNameForPython(parent)
+        else:
+            self.parent = None
+        self.items = {}
+        self.depends = {}
+        models[(self.spec,self.name)] = self
+
+    def AddItem(self, name):
+        parts = tuple(re.sub(r'\.{i}', r'-{i}', name).split('.'))
+        self.items[parts] = 1
+
+    def ItemsMatchingPrefix(self, prefix):
+        for i in sorted(self.items.keys()):
+            if (i[:len(prefix)-1] == prefix[:-1] and i != prefix):
+                yield i
+
+    def Objectify(self, name, parent, prefix):
+        assert (not prefix) or (prefix[-1] == '')
+        params = []
+        objs = []
+        out = []
+        for i in self.ItemsMatchingPrefix(prefix):
+            if len(i) == len(prefix) and i[-1] != '':
+                # a parameter of this object
+                params.append(i[-1])
+            elif len(i) == len(prefix) + 1 and i[-1] == '':
+                # a sub-object of this object
+                objs.append((i[len(prefix)-1],
+                             self.Objectify(i[-2], 'Oogle', i)))
+        name = re.sub(r'-{i}', '', name)  # FIXME
+        out.append('class %s(%s):' % (name, parent))
+        for param in params:
+            out.append('  %s = 1' % (param,))
+        for objname,obj in objs:
+            if len(out) > 1:
+                out.append('')
+            out.append(Indented('  ', unicode(obj)))
+        if len(out) == 1:
+            out.append('  pass')
+        return '\n'.join(out)
+
+    def __str__(self):
+        return self.Objectify(self.name,
+                              self.parent or 'BaseClass', ('',))
+
+
+def RenderParameter(model, prefix, xml):
     name = xml.attrib.get('base', xml.attrib.get('name', '<??>'))
-    print '    %s%s' % (prefix, name)
+    model.AddItem('%s%s' % (prefix, name))
 
 
-def RenderObject(prefix, spec, xml):
+def RenderObject(model, prefix, spec, xml):
     name = xml.attrib.get('base', xml.attrib.get('name', '<??>'))
     prefix += name
-    print '    %s (%s)' % (prefix, xml.tag)
+    model.AddItem(prefix)
     for i in xml:
         if i.tag == 'parameter':
-            RenderParameter(prefix, i)
+            RenderParameter(model, prefix, i)
         elif i.tag == 'object':
-            RenderObject(prefix, spec, i)
+            RenderObject(model, prefix, spec, i)
         elif i.tag in ('description', 'uniqueKey'):
           pass
         else:
             raise KeyError(i.tag)
 
 
-def RenderComponent(prefix, spec, xml):
+def RenderComponent(model, prefix, spec, xml):
     for i in xml:
         if i.tag == 'parameter':
-            RenderParameter(prefix, i)
+            RenderParameter(model, prefix, i)
         elif i.tag == 'object':
-            RenderObject(prefix, spec, i)
+            RenderObject(model, prefix, spec, i)
         elif i.tag == 'component':
             refspec, ref = chunks[spec, 'component', i.attrib['ref']]
             refpath = ref.attrib.get('path', ref.attrib.get('name', '<?>'))
-            RenderComponent(prefix, refspec, ref)
+            RenderComponent(model, prefix, refspec, ref)
         elif i.tag in ('profile', 'description'):
             pass
         else:
@@ -149,6 +205,10 @@ def RenderComponent(prefix, spec, xml):
 
 
 def main():
+    print 'class BaseClass: pass'
+    print 'class Oogle: pass'
+    print
+    
     for filename in sys.argv[1:]:
         ParseFile(filename)
     ResolveImports()
@@ -167,11 +227,12 @@ def main():
                                         ObjNameForPython(name))
                 print
                 continue
-            elif parent:
-                print '  class %s(%s):' % (objname, ObjNameForPython(parent))
+            if parent:
+                model = Model(spec, objname, parent=parent)
             else:
-                print '  class %s:' % objname
-            RenderComponent('', refspec, xml)
+                model = Model(spec, objname, parent=None)
+            RenderComponent(model, '', refspec, xml)
+            print Indented('  ', model)
             print
 
 
