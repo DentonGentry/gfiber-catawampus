e048d78d4f659b0c27b793b1fe8ada4dab2f32b8
Avery Pennarun <apenwarr@gmail.com>
2011-09-06 00:52:34 -0400

    traceroute.py: an implementation of Device.IP.Diagnostics.TraceRoute.
    Uses tr.mainloop so it can run in the background.

--- runserver.py
+++ runserver.py
@@ -1,18 +1,39 @@
 #!/usr/bin/python
 # Copyright 2011 Google Inc. All Rights Reserved.
 #
+# TR-069 has mandatory attribute names that don't comply with policy
+#pylint: disable-msg=C6409
+#pylint: disable-msg=W0404
+#
 """The main server process for our TR-069 CPE device."""
 
-__author__ = 'dgentry@google.com (Denton Gentry)'
+__author__ = 'apenwarr@google.com (Avery Pennarun)'
 
 import device_info
+import tr.core
 import tr.mainloop
-import tr.rcommand  #pylint: disable-msg=W0404
+import tr.rcommand
+import traceroute
+
+
+class Root(tr.core.Exporter):
+  """A fake class that doesn't represent any real device model at all.
+
+  Eventually, we'll replace this with one derived from a real toplevel model
+  in one of the TR docs.
+  """
+
+  def __init__(self, loop):
+    tr.core.Exporter.__init__(self)
+    self.DeviceInfo = device_info.DeviceInfo()
+    self.TraceRoute = traceroute.TraceRoute(loop)
+    self.Export(objects=['DeviceInfo',
+                         'TraceRoute'])
 
 
 def main():
   loop = tr.mainloop.MainLoop()
-  root = device_info.DeviceInfo()
+  root = Root(loop)
   loop.ListenInet6(('', 12999),
                    tr.rcommand.MakeRemoteCommandStreamer(root))
   loop.ListenUnix('/tmp/mainloop.sock',
---
+++ traceroute_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+#
+# Copyright 2011 Google Inc. All Rights Reserved.
+#
+# unittest requires method names starting in 'test'
+#pylint: disable-msg=C6409
+
+"""Unit tests for TraceRoute implementation."""
+
+__author__ = 'apenwarr@google.com (Avery Pennarun)'
+
+import unittest
+import tr.mainloop
+import traceroute
+
+
+class TraceRouteTest(unittest.TestCase):
+  """Tests for traceroute.py."""
+
+  def _DoTrace(self, loop, trace, hostname, maxhops):
+    trace.Host = hostname
+    trace.MaxHopCount = maxhops
+    trace.DiagnosticsState = 'Requested'
+    while trace.DiagnosticsState == 'Requested':
+      loop.RunOnce(timeout=5)
+
+  def testTraceRoute(self):
+    loop = tr.mainloop.MainLoop()
+    trace = traceroute.TraceRoute(loop)
+
+    self._DoTrace(loop, trace, '127.0.0.1', 1)
+    self.assertEqual(len(trace.RouteHopsList), 1)
+    self.assertEqual(trace.RouteHopsList[1].Host, 'localhost')
+    self.assertEqual(trace.RouteHopsList[1].HostAddress, '127.0.0.1')
+    self.assertEqual(trace.DiagnosticsState, 'Error_MaxHopCountExceeded')
+
+    self._DoTrace(loop, trace, '::1', 2)
+    self.assertEqual(len(trace.RouteHopsList), 1)
+    self.assertEqual(trace.RouteHopsList[1].Host, 'localhost')
+    self.assertEqual(trace.RouteHopsList[1].HostAddress, '::1')
+    self.assertEqual(trace.DiagnosticsState, 'Complete')
+
+    self._DoTrace(loop, trace, 'this-name-does-not-exist', 30)
+    self.assertEqual(len(trace.RouteHopsList), 0)
+    self.assertEqual(trace.DiagnosticsState, 'Error_CannotResolveHostName')
+
+    self._DoTrace(loop, trace, 'google.com', 30)
+    self.assertTrue(len(trace.RouteHopsList) > 1)
+    self.assertEqual(trace.DiagnosticsState, 'Complete')
+
+
+if __name__ == '__main__':
+  unittest.main()
