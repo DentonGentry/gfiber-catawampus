a3b49c23a44a0e190d20fb5bd7af1c992af78c35
Denton Gentry <dgentry@google.com>
2011-10-03 05:26:08 -0700

    Don't use accessor functions, populate properties. Check ValidateExport() in unit test.

--- ethernet.py
+++ ethernet.py
@@ -14,17 +14,20 @@ in http://www.broadband-forum.org/cwmp/tr-181-2-2-0.html
 __author__ = 'dgentry@google.com (Denton Gentry)'
 
 import string
+import subprocess
 import tr.core
 import tr.tr181_v2_2 as tr181
 
 BASEETHERNET = tr181.Device_v2_2.Device.Ethernet
 
 class EthernetInterface(BASEETHERNET.Interface):
-  def __init__(self, ifname, upstream):
+  def __init__(self, ifname, upstream,
+               path_to_ethtool='/usr/sbin/ethtool'):
     BASEETHERNET.Interface.__init__(self)
     self.Name = ifname
     self.Upstream = upstream
     self.Stats = EthernetInterfaceStatsLinux26(ifname)
+
 
 
 class EthernetInterfaceStatsLinux26(BASEETHERNET.Interface.Stats):
@@ -47,7 +50,31 @@ class EthernetInterfaceStatsLinux26(BASEETHERNET.Interface.Stats):
 
   def __init__(self, ifname, proc_net_dev='/proc/net/dev'):
     BASEETHERNET.Interface.Stats.__init__(self)
-    self._net_devices = self._ReadProcNetDev(ifname, proc_net_dev)
+    ifstats = self._ReadProcNetDev(ifname, proc_net_dev)
+    self.BroadcastPacketsReceived = None
+    self.BroadcastPacketsSent = None
+    self.BytesReceived = ifstats[self._RX_BYTES]
+    self.BytesSent = ifstats[self._TX_BYTES]
+    self.DiscardPacketsReceived = ifstats[self._RX_DROP]
+    self.DiscardPacketsSent = ifstats[self._TX_DROP]
+
+    err = int(ifstats[self._RX_ERRS]) + int(ifstats[self._RX_FRAME])
+    self.ErrorsReceived = str(err)
+
+    self.ErrorsSent = ifstats[self._TX_FIFO]
+    self.MulticastPacketsReceived = ifstats[self._RX_MCAST]
+    self.MulticastPacketsSent = None
+    self.PacketsReceived = ifstats[self._RX_PKTS]
+    self.PacketsSent = ifstats[self._TX_PKTS]
+
+    rx = int(ifstats[self._RX_PKTS]) - int(ifstats[self._RX_MCAST])
+    self.UnicastPacketsReceived = str(rx)
+
+    # Linux doesn't break out transmit uni/multi/broadcast, but we don't
+    # want to return None for all of them. So we return all transmitted
+    # packets as unicast, though some were surely multicast or broadcast.
+    self.UnicastPacketsSent = ifstats[self._TX_PKTS]
+    self.UnknownProtoPacketsReceived = None
 
   def _ReadProcNetDev(self, ifname, proc_net_dev):
     f = open(proc_net_dev)
@@ -55,60 +82,7 @@ class EthernetInterfaceStatsLinux26(BASEETHERNET.Interface.Stats):
     for line in f:
       fields = line.split(':')
       if (len(fields) == 2) and (fields[0].strip() == ifname):
-        self._ifstats = fields[1].split()
-    return devices
-
-  def BroadcastPacketsReceived(self):
-    return None
-
-  def BroadcastPacketsSent(self):
-    # TODO(dgentry) - Linux doesn't track TX Broadcast.
-    return None
-
-  def BytesReceived(self):
-    return self._ifstats[self._RX_BYTES]
-
-  def BytesSent(self):
-    return self._ifstats[self._TX_BYTES]
-
-  def DiscardPacketsReceived(self):
-    return self._ifstats[self._RX_DROP]
-
-  def DiscardPacketsSent(self):
-    return self._ifstats[self._TX_DROP]
-
-  def ErrorsReceived(self):
-    err = int(self._ifstats[self._RX_ERRS]) + int(self._ifstats[self._RX_FRAME])
-    return str(err)
-
-  def ErrorsSent(self):
-    return self._ifstats[self._TX_FIFO]
-
-  def MulticastPacketsReceived(self):
-    return self._ifstats[self._RX_MCAST]
-
-  def MulticastPacketsSent(self):
-    return None
-
-  def PacketsReceived(self):
-    return self._ifstats[self._RX_PKTS]
-
-  def PacketsSent(self):
-    return self._ifstats[self._TX_PKTS]
-
-  def UnicastPacketsReceived(self):
-    rx = int(self._ifstats[self._RX_PKTS]) - int(self._ifstats[self._RX_MCAST])
-    return str(rx)
-
-  def UnicastPacketsSent(self):
-    # Linux doesn't break out transmit uni/multi/broadcast, but we don't
-    # want to return None for all of them. So we return all transmitted
-    # packets as unicast, though some were surely multicast or broadcast.
-    return self._ifstats[self._TX_PKTS]
-
-  def UnknownProtoPacketsReceived(self):
-    return None
-
+        return fields[1].split()
 
 
 def main():
--- ethernet_test.py
+++ ethernet_test.py
@@ -20,21 +20,26 @@ class EthernetTest(unittest.TestCase):
   def testGoodIfName(self):
     eth = ethernet.EthernetInterfaceStatsLinux26("foo0",
                                                  "testdata/ethernet/net_dev")
-    self.assertEqual(eth.BroadcastPacketsReceived(), None)
-    self.assertEqual(eth.BroadcastPacketsSent(), None)
-    self.assertEqual(eth.BytesReceived(), '1')
-    self.assertEqual(eth.BytesSent(), '9')
-    self.assertEqual(eth.DiscardPacketsReceived(), '4')
-    self.assertEqual(eth.DiscardPacketsSent(), '11')
-    self.assertEqual(eth.ErrorsReceived(), '9')
-    self.assertEqual(eth.ErrorsSent(), '12')
-    self.assertEqual(eth.MulticastPacketsReceived(), '8')
-    self.assertEqual(eth.MulticastPacketsSent(), None)
-    self.assertEqual(eth.PacketsReceived(), '100')
-    self.assertEqual(eth.PacketsSent(), '10')
-    self.assertEqual(eth.UnicastPacketsReceived(), '92')
-    self.assertEqual(eth.UnicastPacketsSent(), '10')
-    self.assertEqual(eth.UnknownProtoPacketsReceived(), None)
+    try:
+      eth.ValidateExports()
+    except SchemaError:
+      self.AssertTrue(False)
+
+    self.assertEqual(eth.BroadcastPacketsReceived, None)
+    self.assertEqual(eth.BroadcastPacketsSent, None)
+    self.assertEqual(eth.BytesReceived, '1')
+    self.assertEqual(eth.BytesSent, '9')
+    self.assertEqual(eth.DiscardPacketsReceived, '4')
+    self.assertEqual(eth.DiscardPacketsSent, '11')
+    self.assertEqual(eth.ErrorsReceived, '9')
+    self.assertEqual(eth.ErrorsSent, '12')
+    self.assertEqual(eth.MulticastPacketsReceived, '8')
+    self.assertEqual(eth.MulticastPacketsSent, None)
+    self.assertEqual(eth.PacketsReceived, '100')
+    self.assertEqual(eth.PacketsSent, '10')
+    self.assertEqual(eth.UnicastPacketsReceived, '92')
+    self.assertEqual(eth.UnicastPacketsSent, '10')
+    self.assertEqual(eth.UnknownProtoPacketsReceived, None)
 
   def testRealLinuxCorpus(self):
     # A test using a /proc/net/dev line taken from a running Linux 2.6.32
@@ -42,21 +47,30 @@ class EthernetTest(unittest.TestCase):
     # using the foo0 fake data instead.
     eth = ethernet.EthernetInterfaceStatsLinux26("eth0",
                                                  "testdata/ethernet/net_dev")
-    self.assertEqual(eth.BroadcastPacketsReceived(), None)
-    self.assertEqual(eth.BroadcastPacketsSent(), None)
-    self.assertEqual(eth.BytesReceived(), '21052761139')
-    self.assertEqual(eth.BytesSent(), '10372833035')
-    self.assertEqual(eth.DiscardPacketsReceived(), '0')
-    self.assertEqual(eth.DiscardPacketsSent(), '0')
-    self.assertEqual(eth.ErrorsReceived(), '0')
-    self.assertEqual(eth.ErrorsSent(), '0')
-    self.assertEqual(eth.MulticastPacketsReceived(), '0')
-    self.assertEqual(eth.MulticastPacketsSent(), None)
-    self.assertEqual(eth.PacketsReceived(), '91456760')
-    self.assertEqual(eth.PacketsSent(), '80960002')
-    self.assertEqual(eth.UnicastPacketsReceived(), '91456760')
-    self.assertEqual(eth.UnicastPacketsSent(), '80960002')
-    self.assertEqual(eth.UnknownProtoPacketsReceived(), None)
+    try:
+      eth.ValidateExports()
+    except SchemaError:
+      self.AssertTrue(False)
+
+    self.assertEqual(eth.BroadcastPacketsReceived, None)
+    self.assertEqual(eth.BroadcastPacketsSent, None)
+    self.assertEqual(eth.BytesReceived, '21052761139')
+    self.assertEqual(eth.BytesSent, '10372833035')
+    self.assertEqual(eth.DiscardPacketsReceived, '0')
+    self.assertEqual(eth.DiscardPacketsSent, '0')
+    self.assertEqual(eth.ErrorsReceived, '0')
+    self.assertEqual(eth.ErrorsSent, '0')
+    self.assertEqual(eth.MulticastPacketsReceived, '0')
+    self.assertEqual(eth.MulticastPacketsSent, None)
+    self.assertEqual(eth.PacketsReceived, '91456760')
+    self.assertEqual(eth.PacketsSent, '80960002')
+    self.assertEqual(eth.UnicastPacketsReceived, '91456760')
+    self.assertEqual(eth.UnicastPacketsSent, '80960002')
+    self.assertEqual(eth.UnknownProtoPacketsReceived, None)
+
+  def testNonexistentIfname(self):
+    self.assertRaises(TypeError, ethernet.EthernetInterfaceStatsLinux26,
+                      "doesnotexist0", "testdata/ethernet/net_dev")
 
 
 if __name__ == '__main__':
