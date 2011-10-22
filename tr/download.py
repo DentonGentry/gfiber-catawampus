f75d4be7f60c2332cf27f377658b403ffdf1bf17
Denton Gentry <dgentry@google.com>
2011-10-22 07:38:34 -0700

    Make download() an explicit method, not a side-effect of the constructor.

--- tr/download.py
+++ tr/download.py
@@ -14,7 +14,7 @@ import tornado.ioloop
 import tornado.web
 
 class HttpDownload(object):
-  def __init__(self, ioloop, command_key=None, file_type=None, url=None,
+  def download(self, ioloop, command_key=None, file_type=None, url=None,
                username=None, password=None, file_size=0, target_filename=None,
                delay_seconds=0):
     self.ioloop = ioloop
@@ -25,11 +25,16 @@ class HttpDownload(object):
     self.password = password
     self.file_size = file_size
     self.target_filename = target_filename
+    self.download_start_time = None
+    self.download_complete_time = None
 
     self.tempfile = tempfile.NamedTemporaryFile(delete=False)
 
-    # I dislike when APIs that demand NTP-related bugs in my code.
+    # I dislike when APIs require NTP-related bugs in my code.
     ioloop.add_timeout(time.time() + delay_seconds, self.delay)
+
+    # tr-69 DownloadResponse: 1 = Download has not yet been completed and applied
+    return 1
 
   def delay(self):
     req = tornado.httpclient.HTTPRequest(
@@ -41,6 +46,7 @@ class HttpDownload(object):
         allow_ipv6 = True)
     http_client = tornado.httpclient.AsyncHTTPClient()
     http_client.fetch(req, self.async_callback)
+    self.download_start_time = time.time()
 
   def streaming_callback(self, data):
     self.tempfile.write(data)
