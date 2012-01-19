722ea08c9d03a7c27d46780d75d4a530ddaf4d98
Avery Pennarun <apenwarr@gmail.com>
2012-01-19 15:29:37 -0800

    Fix "runserver --no-cpe" and rclient.py to work again.
    rclient fix is related to earlier path munging for bup and tornado.
    runserver --no-cpe probably didn't work ever since http statefulness was
    added; we have to disable the http server if there's no CPE (obviously).

--- runserver.py
+++ runserver.py
@@ -90,10 +90,11 @@ def main():
     else:
       acs_url_file = _WriteAcsFile('')
 
-    cpe_machine = tr.http.Listen(opt.ip, opt.port, opt.ping_path, acs,
-                                 acs_url_file, cpe, cpe and opt.cpe_listener)
-    root.add_management_server(cpe_machine.GetManagementServer())
-    cpe_machine.Startup()
+    if cpe:
+      cpe_machine = tr.http.Listen(opt.ip, opt.port, opt.ping_path, acs,
+                                   acs_url_file, cpe, opt.cpe_listener)
+      root.add_management_server(cpe_machine.GetManagementServer())
+      cpe_machine.Startup()
 
   loop.Start()
 
--- tr/rclient.py
+++ tr/rclient.py
@@ -8,12 +8,14 @@
 
 __author__ = 'apenwarr@google.com (Avery Pennarun)'
 
+import sys
+sys.path.append('vendor/tornado')
+sys.path.append('vendor/bup/lib')
 
 import os.path
 import re
 import readline
 import socket
-import sys
 import traceback
 import bup.shquote
 import mainloop
