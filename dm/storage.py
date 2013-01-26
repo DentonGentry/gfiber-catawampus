5d8a33127f543ea653600494ff516c34f388322e
Denton Gentry <dgentry@google.com>
2013-01-26 13:06:54 -0800

    Export more detailed disk information
    Export individual statistics about drive health from smartctl.
    Some of these will be useful in predicting drive failure.
    
    Export SATA PHY statistics. Some of these will be useful in
    diagnosing faulty SATA cables.
    
    Export Linux drive performance statistics.
    
    Make hard drive temperature monitoring use hdd-temperature.
    Invoking hdparm directly only works on Hitachi drives.
    Include hdd-temperature in sh for opensource users (of which
    currently, and probably forever, there are zero).
    
    Fix lint errors.
    
    b/8069921
    b/8071504
    
    Change-Id: I68ff1aa87a8bb2117f1ae936e0e3fe60948cfabc

--- dm/storage.py
+++ dm/storage.py
@@ -1,4 +1,3 @@
-#/usr/bin/python
 # Copyright 2012 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +13,7 @@
 # limitations under the License.
 
 # TR-069 has mandatory attribute names that don't comply with policy
-#pylint: disable-msg=C6409
+# pylint: disable-msg=C6409
 
 """Implementation of tr-140 Storage Services objects."""
 
@@ -34,7 +33,19 @@ import tr.types
 import tr.x_catawampus_storage_1_0
 
 
-BASESTORAGE = tr.x_catawampus_storage_1_0.X_CATAWAMPUS_ORG_Storage_v1_0.StorageService
+CATASTORAGE = tr.x_catawampus_storage_1_0.X_CATAWAMPUS_ORG_Storage_v1_0
+BASESTORAGE = CATASTORAGE.StorageService
+PHYSICALMEDIUM = BASESTORAGE.PhysicalMedium
+DISKSTAT = '/sys/block/'
+
+# Example: 0x0001  2            0  Command failed due to ICRC error
+SATAPHY = re.compile(r'^(?P<reg>\S+)\s+\d+\s+(?P<val>\d+)\s+')
+
+# Example: 1 Raw_Read_Error_Rate 0x000b 100 100 016 Pre-fail Always - 10
+SMARTATTR = re.compile(r'^\s*(\d+)\s+(?P<attr>\S+)\s+\S+\s+\d+\s+\d+\s+\d+'
+                       r'\s+\S+\s+\S+\s+\S+\s+(?P<val>\d+)\s*'
+                       r'(\(Average (?P<average>\d+)\)|'
+                       r'\(Min/Max (?P<min>\d+)/(?P<max>\d+)\))?')
 
 
 class MtdEccStats(ctypes.Structure):
@@ -70,7 +81,7 @@ GETMTDSTATS = _GetMtdStats
 PROC_FILESYSTEMS = '/proc/filesystems'
 PROC_MOUNTS = '/proc/mounts'
 SLASHDEV = '/dev/'
-SMARTCTL = '/usr/sbin/smartctl'
+SMARTCTL = 'smartctl'
 STATVFS = os.statvfs
 SYS_BLOCK = '/sys/block/'
 SYS_UBI = '/sys/class/ubi/'
@@ -95,7 +106,7 @@ def _IsSillyFilesystem(fstype):
 
 def _GetFieldFromOutput(prefix, output, default=''):
   """Search output for line of the form 'Foo: Bar', return 'Bar'."""
-  field_re = re.compile(prefix + '\s*(\S+)')
+  field_re = re.compile(prefix + r'\s*(\S+)')
   for line in output.splitlines():
     result = field_re.search(line)
     if result is not None:
@@ -121,6 +132,14 @@ def IntFromFile(filename):
     return 0
 
 
+def _IntOrNegativeOne(result, name):
+  try:
+    string = result.group(name)
+    return int(string, base=0)
+  except (ValueError, KeyError):
+    return -1
+
+
 class LogicalVolumeLinux26(BASESTORAGE.LogicalVolume):
   """Implementation of tr-140 StorageService.LogicalVolume for Linux FS."""
 
@@ -130,7 +149,7 @@ class LogicalVolumeLinux26(BASESTORAGE.LogicalVolume):
   Status = tr.types.ReadOnlyString('Online')
 
   def __init__(self, rootpath, fstype):
-    BASESTORAGE.LogicalVolume.__init__(self)
+    super(LogicalVolumeLinux26, self).__init__()
     type(self).Name.Set(self, rootpath)
     self.rootpath = rootpath
     type(self).FileSystem.Set(self, fstype)
@@ -179,23 +198,26 @@ class PhysicalMediumDiskLinux26(BASESTORAGE.PhysicalMedium):
 
   CONNECTION_TYPES = frozenset(
       ['USB 1.1', 'USB 2.0', 'IEEE1394', 'IEEE1394b', 'IDE', 'EIDE',
-       'ATA/33', 'ATA/66', 'ATA/100', 'ATA/133', 'SATA/150', 'SATA/300',
+       'ATA/33', 'ATA/66', 'ATA/100', 'ATA/133',
+       'SATA/150', 'SATA/300', 'SATA/600',
        'SCSI-1', 'Fast SCSI', 'Fast-Wide SCSI', 'Ultra SCSI', 'Ultra Wide SCSI',
        'Ultra2 SCSI', 'Ultra2 Wide SCSI', 'Ultra3 SCSI', 'Ultra-320 SCSI',
        'Ultra-640 SCSI', 'SSA', 'SSA-40', 'Fibre Channel'])
 
   ConnectionType = tr.types.ReadOnlyString('')
+  FirmwareVersion = tr.types.ReadOnlyString('')
   Removable = tr.types.ReadOnlyBool(False)
+  SerialNumber = tr.types.ReadOnlyString('')
+  SMARTCapable = tr.types.ReadOnlyBool(False)
 
   def __init__(self, dev, conn_type=None):
-    BASESTORAGE.PhysicalMedium.__init__(self)
+    super(PhysicalMediumDiskLinux26, self).__init__()
     self.dev = dev
-    self.name = dev
+    self.Name = dev
     self.Unexport('Alias')
-    # TODO(dgentry) read SMART attribute for PowerOnHours
-    self.Unexport('Uptime')
     # TODO(dgentry) What does 'Standby' or 'Offline' mean?
     self.Unexport('Status')
+
     if conn_type is None:
       # transport is really, really hard to infer programatically.
       # If platform code doesn't provide it, don't try to guess.
@@ -205,6 +227,28 @@ class PhysicalMediumDiskLinux26(BASESTORAGE.PhysicalMedium):
       # or define a vendor extension. Don't just make something up.
       assert conn_type[0:1] == 'X_' or conn_type in self.CONNECTION_TYPES
     type(self).ConnectionType.Set(self, conn_type)
+    if not conn_type or 'SATA' not in conn_type:
+      self.Unexport(objects='X_CATAWAMPUS-ORG_SataPHY')
+
+    smartctl = self._GetSmartctlOutput()
+    serial = _GetFieldFromOutput(
+        prefix='Serial Number:', output=smartctl, default='')
+    type(self).SerialNumber.Set(self, serial)
+    firmware = _GetFieldFromOutput(
+        prefix='Firmware Version:', output=smartctl, default='')
+    type(self).FirmwareVersion.Set(self, firmware)
+    capable = _GetFieldFromOutput(
+        prefix='SMART support is: Enab', output=smartctl, default=False)
+    smartok = True if capable else False
+    type(self).SMARTCapable.Set(self, smartok)
+    if not smartok:
+      self.Unexport(objects='X_CATAWAMPUS-ORG_SmartAttributes')
+
+  @property
+  def Uptime(self):
+    if self.SMARTCapable:
+      return self.X_CATAWAMPUS_ORG_SmartAttributes.PowerOnHours
+    return 0
 
   @tr.session.cache
   def _GetSmartctlOutput(self):
@@ -214,14 +258,6 @@ class PhysicalMediumDiskLinux26(BASESTORAGE.PhysicalMedium):
                              stdout=subprocess.PIPE)
     out, _ = smart.communicate(None)
     return out
-
-  def GetName(self):
-    return self.name
-
-  def SetName(self, value):
-    self.name = value
-
-  Name = property(GetName, SetName, None, 'PhysicalMedium.Name')
 
   @property
   def Vendor(self):
@@ -236,18 +272,6 @@ class PhysicalMediumDiskLinux26(BASESTORAGE.PhysicalMedium):
     return _ReadOneLine(filename=filename, default='')
 
   @property
-  def SerialNumber(self):
-    return _GetFieldFromOutput(prefix='Serial Number:',
-                               output=self._GetSmartctlOutput(),
-                               default='')
-
-  @property
-  def FirmwareVersion(self):
-    return _GetFieldFromOutput(prefix='Firmware Version:',
-                               output=self._GetSmartctlOutput(),
-                               default='')
-
-  @property
   def Capacity(self):
     """Return capacity in Megabytes."""
     filename = SYS_BLOCK + '/' + self.dev + '/size'
@@ -259,18 +283,10 @@ class PhysicalMediumDiskLinux26(BASESTORAGE.PhysicalMedium):
       return 0
 
   @property
-  def SMARTCapable(self):
-    capable = _GetFieldFromOutput(prefix='SMART support is: Enab',
-                                  output=self._GetSmartctlOutput(),
-                                  default=None)
-    return True if capable else False
-
-  @property
   def Health(self):
     health = _GetFieldFromOutput(
         prefix='SMART overall-health self-assessment test result:',
-        output=self._GetSmartctlOutput(),
-        default='')
+        output=self._GetSmartctlOutput(), default='')
     if health == 'PASSED':
       return 'OK'
     elif health.find('FAIL') >= 0:
@@ -284,12 +300,146 @@ class PhysicalMediumDiskLinux26(BASESTORAGE.PhysicalMedium):
     removable = _ReadOneLine(filename=filename, default='0').strip()
     return False if removable == '0' else True
 
+  @property
+  def X_CATAWAMPUS_ORG_DrivePerformance(self):
+    return DrivePerformance(self.dev)
+
+  @property
+  def X_CATAWAMPUS_ORG_SmartAttributes(self):
+    return SmartAttributes(self.dev)
+
+  @property
+  def X_CATAWAMPUS_ORG_SataPHY(self):
+    return SataPHY(self.dev)
+
+
+class SmartAttributes(PHYSICALMEDIUM.X_CATAWAMPUS_ORG_SmartAttributes):
+  """Catawampus Storage SMART Attributes implementation."""
+
+  CalibrationRetryCount = tr.types.ReadOnlyInt(-1)
+  CurrentPendingSector = tr.types.ReadOnlyInt(-1)
+  FlyingHeight = tr.types.ReadOnlyInt(-1)
+  HardwareEccRecovered = tr.types.ReadOnlyInt(-1)
+  LoadCycleCount = tr.types.ReadOnlyInt(-1)
+  MultiZoneErrorRate = tr.types.ReadOnlyInt(-1)
+  OfflineSeekPerformance = tr.types.ReadOnlyInt(-1)
+  OfflineUncorrectable = tr.types.ReadOnlyInt(-1)
+  PowerCycleCount = tr.types.ReadOnlyInt(-1)
+  PowerOffRetractCount = tr.types.ReadOnlyInt(-1)
+  PowerOnHours = tr.types.ReadOnlyInt(-1)
+  RawReadErrorRate = tr.types.ReadOnlyInt(-1)
+  ReadChannelMargin = tr.types.ReadOnlyInt(-1)
+  ReallocatedEventCount = tr.types.ReadOnlyInt(-1)
+  ReallocatedSectorsCount = tr.types.ReadOnlyInt(-1)
+  RunOutCancel = tr.types.ReadOnlyInt(-1)
+  SeekErrorRate = tr.types.ReadOnlyInt(-1)
+  SeekTimePerformance = tr.types.ReadOnlyInt(-1)
+  ShockCountWriteOperation = tr.types.ReadOnlyInt(-1)
+  ShockRateWriteOperation = tr.types.ReadOnlyInt(-1)
+  SoftReadErrorRate = tr.types.ReadOnlyInt(-1)
+  SpinBuzz = tr.types.ReadOnlyInt(-1)
+  SpinHighCurrent = tr.types.ReadOnlyInt(-1)
+  SpinRetryCount = tr.types.ReadOnlyInt(-1)
+  SpinUpTime = tr.types.ReadOnlyInt(-1)
+  SpinUpTimeLatest = tr.types.ReadOnlyInt(-1)
+  StartStopCount = tr.types.ReadOnlyInt(-1)
+  TaIncreaseCount = tr.types.ReadOnlyInt(-1)
+  TemperatureCelsius = tr.types.ReadOnlyInt(-1)
+  TemperatureCelsiusMax = tr.types.ReadOnlyInt(-1)
+  TemperatureCelsiusMin = tr.types.ReadOnlyInt(-1)
+  ThroughputPerformance = tr.types.ReadOnlyInt(-1)
+  UdmaCrcErrorCount = tr.types.ReadOnlyInt(-1)
+
+  def __init__(self, dev):
+    super(SmartAttributes, self).__init__()
+    self.dev = dev
+    self._ParseSmartctlAttributes()
+
+  def _ParseSmartctlAttributes(self):
+    dev = SLASHDEV + self.dev
+    smart = subprocess.Popen([SMARTCTL, '--attributes', dev],
+                             stdout=subprocess.PIPE)
+    out, _ = smart.communicate(None)
+    for line in out.splitlines():
+      result = SMARTATTR.search(line)
+      if result is None:
+        continue
+      attribute = result.group('attr')
+      value = _IntOrNegativeOne(result, 'val')
+
+      if attribute == 'Calibration_Retry_Count':
+        type(self).CalibrationRetryCount.Set(self, value)
+      elif attribute == 'Current_Pending_Sector':
+        type(self).CurrentPendingSector.Set(self, value)
+      elif attribute == 'Flying_Height':
+        type(self).FlyingHeight.Set(self, value)
+      elif attribute == 'Hardware_ECC_Recovered':
+        type(self).HardwareEccRecovered.Set(self, value)
+      elif attribute == 'Load_Cycle_Count':
+        type(self).LoadCycleCount.Set(self, value)
+      elif attribute == 'Multi_Zone_Error_Rate':
+        type(self).MultiZoneErrorRate.Set(self, value)
+      elif attribute == 'Offline_Seek_Performnce':
+        type(self).OfflineSeekPerformance.Set(self, value)
+      elif attribute == 'Offline_Uncorrectable':
+        type(self).OfflineUncorrectable.Set(self, value)
+      elif attribute == 'Power_Cycle_Count':
+        type(self).PowerCycleCount.Set(self, value)
+      elif attribute == 'Power-Off_Retract_Count':
+        type(self).PowerOffRetractCount.Set(self, value)
+      elif attribute == 'Power_On_Hours':
+        type(self).PowerOnHours.Set(self, value)
+      elif attribute == 'Raw_Read_Error_Rate':
+        type(self).RawReadErrorRate.Set(self, value)
+      elif attribute == 'Read_Channel_Margin':
+        type(self).ReadChannelMargin.Set(self, value)
+      elif attribute == 'Reallocated_Event_Count':
+        type(self).ReallocatedEventCount.Set(self, value)
+      elif attribute == 'Reallocated_Sector_Ct':
+        type(self).ReallocatedSectorsCount.Set(self, value)
+      elif attribute == 'Run_Out_Cancel':
+        type(self).RunOutCancel.Set(self, value)
+      elif attribute == 'Seek_Error_Rate':
+        type(self).SeekErrorRate.Set(self, value)
+      elif attribute == 'Seek_Time_Performance':
+        type(self).SeekTimePerformance.Set(self, value)
+      elif attribute == 'Shock_Count_Write_Opern':
+        type(self).ShockCountWriteOperation.Set(self, value)
+      elif attribute == 'Shock_Rate_Write_Opern':
+        type(self).ShockRateWriteOperation.Set(self, value)
+      elif attribute == 'Soft_Read_Error_Rate':
+        type(self).SoftReadErrorRate.Set(self, value)
+      elif attribute == 'Spin_Buzz':
+        type(self).SpinBuzz.Set(self, value)
+      elif attribute == 'Spin_High_Current':
+        type(self).SpinHighCurrent.Set(self, value)
+      elif attribute == 'Spin_Retry_Count':
+        type(self).SpinRetryCount.Set(self, value)
+      elif attribute == 'Spin_Up_Time':
+        type(self).SpinUpTimeLatest.Set(self, value)
+        average = _IntOrNegativeOne(result, 'average')
+        type(self).SpinUpTime.Set(self, average)
+      elif attribute == 'Start_Stop_Count':
+        type(self).StartStopCount.Set(self, value)
+      elif attribute == 'TA_Increase_Count':
+        type(self).TaIncreaseCount.Set(self, value)
+      elif attribute == 'Temperature_Celsius':
+        type(self).TemperatureCelsius.Set(self, value)
+        type(self).TemperatureCelsiusMin.Set(
+            self, _IntOrNegativeOne(result, 'min'))
+        type(self).TemperatureCelsiusMax.Set(
+            self, _IntOrNegativeOne(result, 'max'))
+      elif attribute == 'Throughput_Performance':
+        type(self).ThroughputPerformance.Set(self, value)
+      elif attribute == 'UDMA_CRC_Error_Count':
+        type(self).UdmaCrcErrorCount.Set(self, value)
+
 
 class FlashSubVolUbiLinux26(BASESTORAGE.X_CATAWAMPUS_ORG_FlashMedia.SubVolume):
   """Catawampus Storage Flash SubVolume implementation for UBI volumes."""
 
   def __init__(self, ubivol):
-    BASESTORAGE.X_CATAWAMPUS_ORG_FlashMedia.SubVolume.__init__(self)
+    super(FlashSubVolUbiLinux26, self).__init__()
     self.ubivol = ubivol
 
   @property
@@ -313,7 +463,7 @@ class FlashMediumUbiLinux26(BASESTORAGE.X_CATAWAMPUS_ORG_FlashMedia):
   Name = tr.types.ReadOnlyString('')
 
   def __init__(self, ubiname):
-    BASESTORAGE.X_CATAWAMPUS_ORG_FlashMedia.__init__(self)
+    super(FlashMediumUbiLinux26, self).__init__()
     type(self).Name.Set(self, ubiname)
     self.SubVolumeList = {}
     num = 0
@@ -367,6 +517,110 @@ class FlashMediumUbiLinux26(BASESTORAGE.X_CATAWAMPUS_ORG_FlashMedia):
     return int(ecc.failed)
 
 
+class DrivePerformance(PHYSICALMEDIUM.X_CATAWAMPUS_ORG_DrivePerformance):
+  """Catawampus Storage Drive Performance implementation."""
+
+  IoInProgress = tr.types.ReadOnlyUnsigned(0)
+  IoMilliseconds = tr.types.ReadOnlyUnsigned(0)
+  ReadMilliseconds = tr.types.ReadOnlyUnsigned(0)
+  ReadSectors = tr.types.ReadOnlyUnsigned(0)
+  ReadsCompleted = tr.types.ReadOnlyUnsigned(0)
+  ReadsMerged = tr.types.ReadOnlyUnsigned(0)
+  WeightedIoMilliseconds = tr.types.ReadOnlyUnsigned(0)
+  WriteMilliseconds = tr.types.ReadOnlyUnsigned(0)
+  WriteSectors = tr.types.ReadOnlyUnsigned(0)
+  WritesCompleted = tr.types.ReadOnlyUnsigned(0)
+  WritesMerged = tr.types.ReadOnlyUnsigned(0)
+
+  def __init__(self, dev):
+    super(DrivePerformance, self).__init__()
+    filename = DISKSTAT + dev + '/stat'
+    with open(filename) as f:
+      fields = f.readline().split()
+      if len(fields) >= 11:
+        type(self).ReadsCompleted.Set(self, long(fields[0]))
+        type(self).ReadsMerged.Set(self, long(fields[1]))
+        type(self).ReadSectors.Set(self, long(fields[2]))
+        type(self).ReadMilliseconds.Set(self, long(fields[3]))
+        type(self).WritesCompleted.Set(self, long(fields[4]))
+        type(self).WritesMerged.Set(self, long(fields[5]))
+        type(self).WriteSectors.Set(self, long(fields[6]))
+        type(self).WriteMilliseconds.Set(self, long(fields[7]))
+        type(self).IoInProgress.Set(self, long(fields[8]))
+        type(self).IoMilliseconds.Set(self, long(fields[9]))
+        type(self).WeightedIoMilliseconds.Set(self, long(fields[10]))
+
+
+class SataPHY(PHYSICALMEDIUM.X_CATAWAMPUS_ORG_SataPHY):
+  """Catawampus Storage SATA PHY counters implementation."""
+
+  CmdFailedICRC = tr.types.ReadOnlyInt(-1)
+  DeviceToHostNonDataRetries = tr.types.ReadOnlyInt(-1)
+  HostToDeviceCrcErrors = tr.types.ReadOnlyInt(-1)
+  HostToDeviceNonCrcErrors = tr.types.ReadOnlyInt(-1)
+  PhyRdyToPhyNRdy = tr.types.ReadOnlyInt(-1)
+  RErrDataFis = tr.types.ReadOnlyInt(-1)
+  RErrDeviceToHostDataFis = tr.types.ReadOnlyInt(-1)
+  RErrDeviceToHostNonDataFis = tr.types.ReadOnlyInt(-1)
+  RErrHostToDeviceDataFis = tr.types.ReadOnlyInt(-1)
+  RErrHostToDeviceDataFisCrc = tr.types.ReadOnlyInt(-1)
+  RErrHostToDeviceDataFisNonCrc = tr.types.ReadOnlyInt(-1)
+  RErrHostToDeviceNonDataFis = tr.types.ReadOnlyInt(-1)
+  RErrHostToDeviceNonDataFisCrc = tr.types.ReadOnlyInt(-1)
+  RErrHostToDeviceNonDataFisNonCrc = tr.types.ReadOnlyInt(-1)
+  RErrNonDataFis = tr.types.ReadOnlyInt(-1)
+  RegisterFisComreset = tr.types.ReadOnlyInt(-1)
+
+  def __init__(self, dev):
+    super(SataPHY, self).__init__()
+    self.dev = dev
+    self._ParseSmartctlSataphy()
+
+  def _ParseSmartctlSataphy(self):
+    dev = SLASHDEV + self.dev
+    smart = subprocess.Popen([SMARTCTL, '--log=sataphy', dev],
+                             stdout=subprocess.PIPE)
+    out, _ = smart.communicate(None)
+    for line in out.splitlines():
+      result = SATAPHY.search(line)
+      if result is None:
+        continue
+      reg = _IntOrNegativeOne(result, 'reg')
+      val = _IntOrNegativeOne(result, 'val')
+      if reg == 1:
+        type(self).CmdFailedICRC.Set(self, val)
+      elif reg == 2:
+        type(self).RErrDataFis.Set(self, val)
+      elif reg == 3:
+        type(self).RErrDeviceToHostDataFis.Set(self, val)
+      elif reg == 4:
+        type(self).RErrHostToDeviceDataFis.Set(self, val)
+      elif reg == 5:
+        type(self).RErrNonDataFis.Set(self, val)
+      elif reg == 6:
+        type(self).RErrDeviceToHostNonDataFis.Set(self, val)
+      elif reg == 7:
+        type(self).RErrHostToDeviceNonDataFis.Set(self, val)
+      elif reg == 8:
+        type(self).DeviceToHostNonDataRetries.Set(self, val)
+      elif reg == 9:
+        type(self).PhyRdyToPhyNRdy.Set(self, val)
+      elif reg == 10:
+        type(self).RegisterFisComreset.Set(self, val)
+      elif reg == 11:
+        type(self).HostToDeviceCrcErrors.Set(self, val)
+      elif reg == 13:
+        type(self).HostToDeviceNonCrcErrors.Set(self, val)
+      elif reg == 15:
+        type(self).RErrHostToDeviceDataFisCrc.Set(self, val)
+      elif reg == 16:
+        type(self).RErrHostToDeviceDataFisNonCrc.Set(self, val)
+      elif reg == 18:
+        type(self).RErrHostToDeviceNonDataFisCrc.Set(self, val)
+      elif reg == 19:
+        type(self).RErrHostToDeviceNonDataFisNonCrc.Set(self, val)
+
+
 class CapabilitiesNoneLinux26(BASESTORAGE.Capabilities):
   """Trivial tr-140 StorageService.Capabilities, all False."""
 
@@ -378,9 +632,6 @@ class CapabilitiesNoneLinux26(BASESTORAGE.Capabilities):
   SupportedNetworkProtocols = tr.types.ReadOnlyString('')
   SupportedRaidTypes = tr.types.ReadOnlyString('')
   VolumeEncryptionCapable = tr.types.ReadOnlyBool(False)
-
-  def __init__(self):
-    BASESTORAGE.Capabilities.__init__(self)
 
   @property
   def SupportedFileSystemTypes(self):
@@ -416,7 +667,7 @@ class StorageServiceLinux26(BASESTORAGE):
   Enable = tr.types.ReadOnlyBool(True)
 
   def __init__(self):
-    BASESTORAGE.__init__(self)
+    super(StorageServiceLinux26, self).__init__()
     self.Capabilities = CapabilitiesNoneLinux26()
     self.Unexport('Alias')
     self.Unexport(objects='NetInfo')
@@ -494,10 +745,3 @@ class StorageServiceLinux26(BASESTORAGE):
     if index >= len(fstuples):
       raise IndexError('No such object LogicalVolume.{0}'.format(index))
     return self.GetLogicalVolume(fstuples[index])
-
-
-def main():
-  pass
-
-if __name__ == '__main__':
-  main()
