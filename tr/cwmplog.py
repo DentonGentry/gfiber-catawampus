93659448e3c1127c8431624d90453c3ca8401d35
Denton Gentry <dgentry@google.com>
2013-06-02 18:44:31 -0700

    Let the logging be terse.
    After the first few CWMP messages, switch to logging a condensed description
    of the operation and not the XML.  New sample logs:
    
    RUN
    CPE RECEIVED (at Mon Sep 16 10:42:55 2013):
    ID: google.acs.1379356975610.23095458
    SetParameterValues:
      Device.PeriodicStatistics.SampleSet.43.Parameter.1.Enable = true
      Device.PeriodicStatistics.SampleSet.43.Parameter.1.Reference = Device.Services.STBService.1.ServiceMonitoring.MainStream.7.Total.DejitteringStats.EmptyBufferTime
      Device.PeriodicStatistics.SampleSet.43.Parameter.1.SampleMode = Current
      Device.P.....t.43.Parameter.1.CalculationMode = Latest
      Device.PeriodicStatistics.SampleSet.43.Parameter.2.Enable = true
      Device.PeriodicStatistics.SampleSet.43.Parameter.2.Reference = Device.Services.STBService.1.ServiceMonitoring.MainStream.7.Total.DejitteringStats.Overruns
      Device.PeriodicStatistics.SampleSet.43.Parameter.2.SampleMode = Current
      Device.P.....t.43.Parameter.2.CalculationMode = Latest
    
    RUN
    CPE RECEIVED (at Sat Sep 28 10:00:17 2013):
    Effective URL was: 'https://acs.e2e.gfsvc.com/cwmp'
    ID: google.acs.1380391217066.6126576
    SetParameterAttributes:
      Name: Device.PeriodicStatistics.SampleSet.10.Status
      NotificationChange: true
      Notification: 1
      AccessListChange: false
      AccessList:
    
    Change-Id: Ibcae7bfb20dd830715fa4412d7f15834ca39b0b1

---
+++ tr/cwmplog.py
@@ -0,0 +1,157 @@
+#!/usr/bin/python
+# Copyright 2011 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# TR-069 has mandatory attribute names that don't comply with policy
+# pylint: disable-msg=C6409
+# pylint: disable-msg=W0404
+#
+"""Implement logging for cwmpd."""
+
+
+import os
+import soap
+import xml.etree.ElementTree as ET
+
+
+SUPPRESSLIST = frozenset(['ParameterNames', 'string', 'ParameterList',
+                          'ParameterValueStruct', 'DeviceId', 'Event',
+                          'EventStruct', 'detail', 'Header', 'Body',
+                          'SetParameterAttributesStruct', ])
+PRUNELIST = frozenset(['MaxEnvelopes', 'ParameterKey', 'CommandKey',
+                       'Manufacturer', 'OUI', 'ProductClass',
+                       'HoldRequests', ])
+
+
+def _Shorten(s, prefixofs, suffixofs, maxlen):
+  """Shorten the given string if its length is >= maxlen.
+
+  Note: maxlen should generally be considerably bigger than
+  prefixofs + suffixofs.  It's disconcerting to a reader when
+  you have a "..." to replace 10 bytes, but it feels fine when the
+  "..." replaces 500 bytes.
+
+  Args:
+    s: the string to shorten.
+    prefixofs: the number of chars to keep at the beginning of s.
+    suffixofs: the number of chars to keep at the end of s.
+    maxlen: if the string is longer than this, shorten it.
+  Returns:
+    A shortened version of the string.
+  """
+  s = str(s)
+  if len(s) >= maxlen:
+    # When the string exceeds the limit, we deliberately shorten it to
+    # considerably less than the limit, because it's disconcerting when
+    # you have a "..." to replace 10 bytes, but it feels right when the
+    # "..." replaces 500 bytes.
+    s = s[0:prefixofs] + '.....' + s[-suffixofs:]
+  return s
+
+
+def _StripNamespace(tag):
+  """Remove Namespace from XML Tag.
+
+  ElementTree retains namespaces in the tag, like:
+  {urn:dslforum-org:cwmp-1-2}GetParameterNames
+  The namespace is not useful; suppress it.
+  """
+  if tag[0] == '{':
+    e = tag.find('}')
+    return tag[e+1:]
+  else:
+    return tag
+
+
+def _LogSoapETree(et, prefix=''):
+  """Tersely log an ElementTree CWMP message.
+
+  Example output:
+
+  ID: google.acs.1370212049608.571675
+  SetParameterValues:
+    InternetGatewayDevice.ManagementServer.PeriodicInformEnable = true
+    InternetGatewayDevice.ManagementServer.PeriodicInformInterval = 60
+
+  Args:
+    et: an ElementTree node
+    prefix: a string to prepend to each line (generally indentation)
+  Returns:
+    the string to log.
+  """
+  if len(et) == 2 and et[0].tag == 'Name' and et[1].tag == 'Value':
+    # change:
+    #   Name: InternetGatewayDevice.Foo.Bar
+    #   Value: 60
+    # to:
+    #   InternetGatewayDevice.Foo.Bar = 60
+    text0 = et[0].text and et[0].text.strip()
+    text0 = _Shorten(text0, 8, 32, 64)
+    text1 = et[1].text and et[1].text.strip()
+    text1 = _Shorten(text1, 16, 64, 192)
+    return '%s%s = %s\n' % (prefix, text0, text1)
+  if et.text and et.text.strip():
+    return '%s%s\n' % (prefix, et.text.strip())
+  out = ''
+  for child in et:
+    tag = _StripNamespace(child.tag)
+    if tag in PRUNELIST:
+      et.remove(child)
+      continue
+    if tag in SUPPRESSLIST:
+      out += '%s' % _LogSoapETree(child, prefix)
+    elif child.text and child.text.strip():
+      # Child is a leaf node; all on one line
+      out += '%s%s: %s\n' % (prefix, tag, child.text.strip())
+    else:
+      out += '%s%s:\n%s' % (prefix, tag, _LogSoapETree(child, prefix + '  '))
+  return out
+
+
+class Logger(object):
+  """CWMP-specific logging support.
+
+  This class logs XML logs for the first few sessions after
+  reboot, trying to capture the initial GetParameterValues
+  exchanges and an AddObject or two. Then it switches to a
+  much more condensed logging format.
+  """
+
+  def __init__(self, full_logs=10):
+    self.num_full_logs = full_logs
+
+  def LogSoapXML(self, xml):
+    if not xml or not xml.strip():
+      # An empty message is valid, means connection is closing.
+      return ''
+
+    # Developers can ask for full XML logs.
+    if os.environ.get('DONT_SHORTEN'):
+      return str(xml)
+
+    if self.num_full_logs > 0:
+      self.num_full_logs -= 1
+      return self.LogXML(xml)
+    else:
+      return self.LogTerse(xml)
+
+  def LogXML(self, xml):
+    return _Shorten(str(xml), 1024, 512, 3072)
+
+  def LogTerse(self, xml):
+    try:
+      return _LogSoapETree(ET.fromstring(xml))
+    except:  # Never, ever kill catawampus for this. pylint: disable-msg=W0702
+      print 'Unable to parse XML for logging.'
+      return str(xml)
---
+++ tr/cwmplog_test.py
@@ -0,0 +1,399 @@
+#!/usr/bin/python
+# Copyright 2011 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# unittest requires method names starting in 'test'
+# pylint: disable-msg=C6409
+
+"""Unit tests for cwmpboolean.py."""
+
+__author__ = 'dgentry@google.com (Denton Gentry)'
+
+import os
+import unittest
+
+import google3
+import cwmplog
+
+
+class CwmpLogTest(unittest.TestCase):
+  """Tests for boolean formatting."""
+
+  def setUp(self):
+    if 'DONT_SHORTEN' in os.environ:
+      del os.environ['DONT_SHORTEN']
+
+  def testInform(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(InformXML)
+    self.assertEqual(log, InformLog)
+
+  def testInformResponse(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(InformResponseXML)
+    self.assertEqual(log, InformResponseLog)
+
+  def testSetParameterValues(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(SetParameterValuesXML)
+    self.assertEqual(log, SetParameterValuesLog)
+
+  def testSetParameterValuesResponseSuccess(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(SetParameterValuesResponseSuccessXML)
+    self.assertEqual(log, SetParameterValuesResponseSuccessLog)
+
+  def testGetParameterNames(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(GetParameterNamesXML)
+    self.assertEqual(log, GetParameterNamesLog)
+
+  def testAddObject(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(AddObjectXML)
+    self.assertEqual(log, AddObjectLog)
+
+  def testAddObjectResponse(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(AddObjectResponseXML)
+    self.assertEqual(log, AddObjectResponseLog)
+
+  def testCwmpFault(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(CwmpFaultXML)
+    self.assertEqual(log, CwmpFaultLog)
+
+  def testGetParameterValues(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(GetParameterValuesXML)
+    self.assertEqual(log, GetParameterValuesLog)
+
+  def testGetParameterValuesResponse(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(GetParameterValuesResponseXML)
+    self.assertEqual(log, GetParameterValuesResponseLog)
+
+  def testBadXML(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML('this is <not> XML')
+    self.assertEqual(log, 'this is <not> XML')
+
+  def testEmpty(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML('')
+    self.assertEqual(log, '')
+
+  def testShortening(self):
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(GetParameterValuesResponseLongXML)
+    self.assertEqual(log, GetParameterValuesResponseLongLog)
+
+  def testDontShorten(self):
+    os.environ['DONT_SHORTEN'] = '1'
+    logger = cwmplog.Logger(full_logs=0)
+    log = logger.LogSoapXML(GetParameterValuesResponseLongXML)
+    self.assertEqual(log, GetParameterValuesResponseLongXML)
+
+  def testFullLogs(self):
+    logger = cwmplog.Logger(full_logs=2)
+    self.assertEqual(logger.LogSoapXML(GetParameterValuesResponseLongXML),
+                     GetParameterValuesResponseLongXML)
+    self.assertEqual(logger.LogSoapXML(GetParameterValuesResponseLongXML),
+                     GetParameterValuesResponseLongXML)
+    self.assertEqual(logger.LogSoapXML(GetParameterValuesResponseLongXML),
+                     GetParameterValuesResponseLongLog)
+
+
+# pylint: disable-msg=C6310
+InformXML = """<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:cwmp="urn:dslforum-org:cwmp-1-2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap-enc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <soap:Header>
+    <cwmp:ID soap:mustUnderstand="1">catawampus.1370208445.785391</cwmp:ID>
+  </soap:Header>
+  <soap:Body>
+    <cwmp:Inform>
+      <DeviceId>
+        <Manufacturer>Google Fiber</Manufacturer>
+        <OUI>F88FCA</OUI>
+        <ProductClass>GFHD100</ProductClass>
+        <SerialNumber>G00223301811</SerialNumber>
+      </DeviceId>
+      <Event soap-enc:arrayType="EventStruct[2]">
+        <EventStruct>
+          <EventCode>0 BOOTSTRAP</EventCode>
+          <CommandKey />
+        </EventStruct>
+        <EventStruct>
+          <EventCode>M TRANSFERCOMPLETE</EventCode>
+          <CommandKey />
+        </EventStruct>
+      </Event>
+      <MaxEnvelopes>1</MaxEnvelopes>
+      <CurrentTime>2013-06-02T21:27:26.080821Z</CurrentTime>
+      <RetryCount>0</RetryCount>
+      <ParameterList soap-enc:arrayType="cwmp:ParameterValueStruct[5]">
+        <ParameterValueStruct>
+          <Name>InternetGatewayDevice.ManagementServer.ConnectionRequestURL</Name>
+          <Value xsi:type="xsd:string">http://[2605:a601:fe00:fff7:fa8f:caff:fe00:a28c]:7547/ping/21b19cbcfd821008be9c1938219a1b</Value>
+        </ParameterValueStruct>
+        <ParameterValueStruct>
+          <Name>InternetGatewayDevice.ManagementServer.ParameterKey</Name>
+          <Value xsi:type="xsd:string"></Value>
+        </ParameterValueStruct>
+        <ParameterValueStruct>
+          <Name>InternetGatewayDevice.DeviceInfo.HardwareVersion</Name>
+          <Value xsi:type="xsd:string">2</Value>
+        </ParameterValueStruct>
+        <ParameterValueStruct>
+          <Name>InternetGatewayDevice.DeviceInfo.SoftwareVersion</Name>
+          <Value xsi:type="xsd:string">gfibertv-32-pre0-14-g8ed95de-dg</Value>
+        </ParameterValueStruct>
+        <ParameterValueStruct>
+          <Name>InternetGatewayDevice.DeviceInfo.SpecVersion</Name>
+          <Value xsi:type="xsd:string">1.0</Value>
+        </ParameterValueStruct>
+      </ParameterList>
+    </cwmp:Inform>
+  </soap:Body>
+</soap:Envelope>"""
+
+# pylint: disable-msg=C6308
+InformLog = """ID: catawampus.1370208445.785391
+Inform:
+  EventCode: 0 BOOTSTRAP
+  EventCode: M TRANSFERCOMPLETE
+  RetryCount: 0
+  InternetGatewayDevice.ManagementServer.ConnectionRequestURL = http://[2605:a601:fe00:fff7:fa8f:caff:fe00:a28c]:7547/ping/21b19cbcfd821008be9c1938219a1b
+  InternetGatewayDevice.ManagementServer.ParameterKey = None
+  InternetGatewayDevice.DeviceInfo.HardwareVersion = 2
+  InternetGatewayDevice.DeviceInfo.SoftwareVersion = gfibertv-32-pre0-14-g8ed95de-dg
+  InternetGatewayDevice.DeviceInfo.SpecVersion = 1.0
+"""
+
+InformResponseXML = """<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:cwmp="urn:dslforum-org:cwmp-1-2" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <soapenv:Header>
+    <cwmp:ID soapenv:mustUnderstand="1">catawampus.1370208445.785391</cwmp:ID>
+    <cwmp:HoldRequests soapenv:mustUnderstand="1">1</cwmp:HoldRequests>
+  </soapenv:Header>
+  <soapenv:Body>
+    <cwmp:InformResponse>
+      <MaxEnvelopes>1</MaxEnvelopes>
+    </cwmp:InformResponse>
+  </soapenv:Body>
+</soapenv:Envelope>"""
+
+InformResponseLog = """ID: catawampus.1370208445.785391
+InformResponse:
+"""
+
+SetParameterValuesXML = """<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:cwmp="urn:dslforum-org:cwmp-1-2" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <soapenv:Header>
+    <cwmp:ID soapenv:mustUnderstand="1">google.acs.1370212049608.571675</cwmp:ID>
+    <cwmp:HoldRequests soapenv:mustUnderstand="1">1</cwmp:HoldRequests>
+  </soapenv:Header>
+  <soapenv:Body>
+    <cwmp:SetParameterValues>
+      <ParameterList soapenc:arrayType="cwmp:ParameterValueStruct[2]">
+        <ns5:ParameterValueStruct xmlns:ns1="http://schemas.xmlsoap.org/soap/encoding/" xmlns:ns3="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns4="urn:dslforum-org:cwmp-1-0" xmlns:ns5="urn:dslforum-org:cwmp-1-2">
+          <Name>InternetGatewayDevice.ManagementServer.PeriodicInformEnable</Name>
+          <Value xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:boolean">true</Value>
+        </ns5:ParameterValueStruct>
+        <ns5:ParameterValueStruct xmlns:ns1="http://schemas.xmlsoap.org/soap/encoding/" xmlns:ns3="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns4="urn:dslforum-org:cwmp-1-0" xmlns:ns5="urn:dslforum-org:cwmp-1-2">
+          <Name>InternetGatewayDevice.ManagementServer.PeriodicInformInterval</Name>
+          <Value xsi:type="ns1:unsignedInt">60</Value>
+        </ns5:ParameterValueStruct>
+      </ParameterList>
+      <ParameterKey>GoogleInitConfig</ParameterKey>
+    </cwmp:SetParameterValues>
+  </soapenv:Body>
+</soapenv:Envelope>"""
+
+SetParameterValuesLog = """ID: google.acs.1370212049608.571675
+SetParameterValues:
+  InternetGatewayDevice.ManagementServer.PeriodicInformEnable = true
+  InternetGatewayDevice.ManagementServer.PeriodicInformInterval = 60
+"""
+
+SetParameterValuesResponseSuccessXML = """<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:cwmp="urn:dslforum-org:cwmp-1-2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap-enc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <soap:Header>
+    <cwmp:ID soap:mustUnderstand="1">google.acs.1370212049608.571675</cwmp:ID>
+  </soap:Header>
+  <soap:Body>
+    <cwmp:SetParameterValuesResponse>
+      <Status>0</Status>
+    </cwmp:SetParameterValuesResponse>
+  </soap:Body>
+</soap:Envelope>"""
+
+SetParameterValuesResponseSuccessLog = """ID: google.acs.1370212049608.571675
+SetParameterValuesResponse:
+  Status: 0
+"""
+
+GetParameterNamesXML = """<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:cwmp="urn:dslforum-org:cwmp-1-2" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <soapenv:Header>
+    <cwmp:ID soapenv:mustUnderstand="1">google.acs.1370215481597.9359765</cwmp:ID>
+    <cwmp:HoldRequests soapenv:mustUnderstand="1">0</cwmp:HoldRequests>
+  </soapenv:Header>
+  <soapenv:Body>
+    <cwmp:GetParameterNames>
+      <ParameterPath>Device.PeriodicStatistics.SampleSet.</ParameterPath>
+      <NextLevel>true</NextLevel>
+    </cwmp:GetParameterNames>
+  </soapenv:Body>
+</soapenv:Envelope>"""
+
+GetParameterNamesLog = """ID: google.acs.1370215481597.9359765
+GetParameterNames:
+  ParameterPath: Device.PeriodicStatistics.SampleSet.
+  NextLevel: true
+"""
+
+AddObjectXML = """<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:cwmp="urn:dslforum-org:cwmp-1-2" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <soapenv:Header>
+    <cwmp:ID soapenv:mustUnderstand="1">google.acs.1370215543274.32355095</cwmp:ID>
+    <cwmp:HoldRequests soapenv:mustUnderstand="1">1</cwmp:HoldRequests>
+  </soapenv:Header>
+  <soapenv:Body>
+    <cwmp:AddObject>
+      <ObjectName>Device.PeriodicStatistics.SampleSet.1.Parameter.</ObjectName>
+      <ParameterKey>GoogleInitConfig</ParameterKey>
+    </cwmp:AddObject>
+  </soapenv:Body>
+</soapenv:Envelope>"""
+
+AddObjectLog = """ID: google.acs.1370215543274.32355095
+AddObject:
+  ObjectName: Device.PeriodicStatistics.SampleSet.1.Parameter.
+"""
+
+AddObjectResponseXML = """<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:cwmp="urn:dslforum-org:cwmp-1-2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap-enc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <soap:Header>
+    <cwmp:ID soap:mustUnderstand="1">google.acs.1370215543274.32355095</cwmp:ID>
+  </soap:Header>
+  <soap:Body>
+    <cwmp:AddObjectResponse>
+      <InstanceNumber>2</InstanceNumber>
+      <Status>0</Status>
+    </cwmp:AddObjectResponse>
+  </soap:Body>
+</soap:Envelope>"""
+
+AddObjectResponseLog = """ID: google.acs.1370215543274.32355095
+AddObjectResponse:
+  InstanceNumber: 2
+  Status: 0
+"""
+
+CwmpFaultXML = """<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:cwmp="urn:dslforum-org:cwmp-1-2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap-enc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <soap:Header>
+    <cwmp:ID soap:mustUnderstand="1">google.acs.1370221731262.21249673</cwmp:ID>
+  </soap:Header>
+  <soap:Body>
+    <soap:Fault>
+      <faultcode>Client</faultcode>
+      <faultstring>CWMP fault</faultstring>
+      <detail>
+        <cwmp:Fault>
+          <FaultCode>9005</FaultCode>
+          <FaultString>No such parameter: Info</FaultString>
+        </cwmp:Fault>
+      </detail>
+    </soap:Fault>
+  </soap:Body>
+</soap:Envelope>"""
+
+CwmpFaultLog = """ID: google.acs.1370221731262.21249673
+Fault:
+  faultcode: Client
+  faultstring: CWMP fault
+  Fault:
+    FaultCode: 9005
+    FaultString: No such parameter: Info
+"""
+
+GetParameterValuesXML = """<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:cwmp="urn:dslforum-org:cwmp-1-2" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+  <soapenv:Header>
+    <cwmp:ID soapenv:mustUnderstand="1">google.acs.1370224916205.16224788</cwmp:ID>
+    <cwmp:HoldRequests soapenv:mustUnderstand="1">0</cwmp:HoldRequests>
+  </soapenv:Header>
+  <soapenv:Body>
+    <cwmp:GetParameterValues>
+      <ParameterNames soapenc:arrayType="{urn:dslforum-org:cwmp-1-2}string[1]">
+        <ns5:string xmlns:ns1="http://schemas.xmlsoap.org/soap/encoding/" xmlns:ns3="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns4="urn:dslforum-org:cwmp-1-0" xmlns:ns5="urn:dslforum-org:cwmp-1-2">Device.DeviceInfo.UpTime</ns5:string>
+      </ParameterNames>
+    </cwmp:GetParameterValues>
+  </soapenv:Body>
+</soapenv:Envelope>"""
+
+GetParameterValuesLog = """ID: google.acs.1370224916205.16224788
+GetParameterValues:
+  Device.DeviceInfo.UpTime
+"""
+
+GetParameterValuesResponseXML = """<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:cwmp="urn:dslforum-org:cwmp-1-2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap-enc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <soap:Header>
+    <cwmp:ID soap:mustUnderstand="1">google.acs.1370224916205.16224788</cwmp:ID>
+  </soap:Header>
+  <soap:Body>
+    <cwmp:GetParameterValuesResponse>
+      <ParameterList soap-enc:arrayType="cwmp:ParameterValueStruct[1]">
+        <ParameterValueStruct>
+          <Name>Device.DeviceInfo.UpTime</Name>
+          <Value xsi:type="xsd:unsignedInt">516925</Value>
+        </ParameterValueStruct>
+      </ParameterList>
+    </cwmp:GetParameterValuesResponse>
+  </soap:Body>
+</soap:Envelope>"""
+
+GetParameterValuesResponseLog = """ID: google.acs.1370224916205.16224788
+GetParameterValuesResponse:
+  Device.DeviceInfo.UpTime = 516925
+"""
+
+GetParameterValuesResponseLongXML = """<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:cwmp="urn:dslforum-org:cwmp-1-2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap-enc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <soap:Header>
+    <cwmp:ID soap:mustUnderstand="1">google.acs.1370224916205.16224788</cwmp:ID>
+  </soap:Header>
+  <soap:Body>
+    <cwmp:GetParameterValuesResponse>
+      <ParameterList soap-enc:arrayType="cwmp:ParameterValueStruct[1]">
+        <ParameterValueStruct>
+          <Name>Device.DeviceInfo.This.Is.A.Very.Long.Parameter.Name.Which.Should.Be.Shortened.In.Order.To.Reduce.The.Footprint.Of.The.Logging</Name>
+          <Value xsi:type="xsd:unsignedInt">abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789</Value>
+        </ParameterValueStruct>
+      </ParameterList>
+    </cwmp:GetParameterValuesResponse>
+  </soap:Body>
+</soap:Envelope>"""
+
+GetParameterValuesResponseLongLog = """ID: google.acs.1370224916205.16224788
+GetParameterValuesResponse:
+  Device.D.....uce.The.Footprint.Of.The.Logging = abcdefghijklmnop.....ijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789
+"""
+
+
+if __name__ == '__main__':
+  unittest.main()
