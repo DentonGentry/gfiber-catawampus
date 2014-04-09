695b32eff756f7c4a6bedd66c0af79f628a03f7c
John Newlin <jnewlin@google.com>
2014-04-09 10:48:21 -0700

    Start fixing up some diagui issues
    - No longer redirect to myfiber.
    - Main page displays stats
    - Add a link at the top to myfiber.
    - Display info without requiring an extra click.
    - Change test to not use a tempfile, just update data structures
      directly.  The existing testdata was being used for current results.
    - Fix the ACS last contact time.
    - Add an interval timer to refresh the data every 30s.
    - Default to port 80, use a flag to override.
    - I'm sure there's something else.
    
    Change-Id: Ic6fc0bc3f1d5fcc267b42a22d34a6239fc5c9b82

--- diagui/main.py
+++ diagui/main.py
@@ -13,14 +13,6 @@ import tr.pyinotify
 import tr.types
 
 
-# TODO(anandkhare): conditionally redirect only when needed
-class MainHandler(tornado.web.RequestHandler):
-  """If RG's connectivity to cloud is healthy, redirect to fiber.google.com."""
-
-  def get(self):    # pylint: disable=g-bad-name
-    self.redirect('https://fiber.google.com')
-
-
 class DiagnosticsHandler(tornado.web.RequestHandler):
   """If no connectivity, display local diagnostics UI."""
 
@@ -70,6 +62,7 @@ class DiaguiSettings(tornado.web.Application):
   """Defines settings for the server and notifier."""
 
   def __init__(self, root, cpemach):
+    self.data = {}
     self.root = root
     self.cpemach = cpemach
     if self.root:
@@ -85,8 +78,7 @@ class DiaguiSettings(tornado.web.Application):
         'xsrf_cookies': True,
     }
     super(DiaguiSettings, self).__init__([
-        (r'/', MainHandler),
-        (r'/diagnostics', DiagnosticsHandler),
+        (r'/', DiagnosticsHandler),
         (r'/content.json', JsonHandler),
         (r'/restart', RestartHandler),
     ], **self.settings)
@@ -100,122 +92,127 @@ class DiaguiSettings(tornado.web.Application):
     self.wdd = self.wm.add_watch(
         os.path.join(self.pathname, 'Testdata'), self.mask)
 
+
   def AlertNotifiers(self, obj):
     self.UpdateLatestDict()
     for i in self.callbacklist[:]:
       i()
 
-  def UpdateLatestDict(self):
-    """Updates the dictionary and checksum value."""
 
-    f = open(os.path.join(self.pathname, 'Testdata/testdata'))
-    self.data = dict(line.decode('utf-8').strip().split(None, 1) for line in f)
-    if self.root:
-      self.data['subnetmask'] = ''
-
-      deviceinfo = self.root.Device.DeviceInfo
-      tempstatus = deviceinfo.TemperatureStatus
-      landevlist = self.root.InternetGatewayDevice.LANDeviceList
-      etherlist = self.root.Device.Ethernet.InterfaceList
-
-      if self.cpemach.last_success_response:
-        self.data['acs'] = 'OK (%s)' % self.cpemach.last_success_response
-      else:
-        self.data['acs'] = 'Never Contacted'
-      self.data['softversion'] = deviceinfo.SoftwareVersion
-      self.data['uptime'] = deviceinfo.UpTime
-      self.data['username'] = self.root.Device.ManagementServer.Username
-
-      t = dict()
-      try:
-        for i, sensor in tempstatus.TemperatureSensorList.iteritems():
-          t[sensor.Name] = sensor.Value
-        self.data['temperature'] = t
-      except AttributeError:
-        pass
-
-      t = dict()
-      for i, inter in self.root.Device.IP.InterfaceList.iteritems():
-        for j, ip4 in inter.IPv4AddressList.iteritems():
-          t[ip4.IPAddress] = '(%s)' % ip4.Status
-          self.data['subnetmask'] = ip4.SubnetMask
-        for j, ip6 in inter.IPv6AddressList.iteritems():
-          if ip6.IPAddress[:4] != 'fe80':
-            t[ip6.IPAddress] = '(%s)' % ip6.Status
-      self.data['lanip'] = t
-
-      t = dict()
-      for i, interface in etherlist.iteritems():
-        t[interface.MACAddress] = '(%s)' % interface.Status
-      self.data['wiredlan'] = t
-
-      t = dict()
-      for i, inter in self.root.Device.MoCA.InterfaceList.iteritems():
-        for j, dev in inter.AssociatedDeviceList.iteritems():
-          t[dev.NodeID] = dev.MACAddress
-      self.data['wireddevices'] = t
-
-      wlan = dict()
-      devices = dict()
-      wpa = dict()
-      self.data['ssid5'] = ''
-
-      for i, dev in landevlist.iteritems():
-        for j, wlconf in dev.WLANConfigurationList.iteritems():
-          if wlconf.Channel in range(1, 12):
-            self.data['ssid24'] = wlconf.SSID
-            if wlconf.WPAAuthenticationMode == 'PSKAuthentication':
-              wpa['2.4 GHz'] = '(configured)'
-            wlan[wlconf.BSSID] = '(2.4 GHz) (%s)' % wlconf.Status
-            for k, assoc in wlconf.AssociatedDeviceList.iteritems():
-              devices[assoc.AssociatedDeviceMACAddress] = (
-                  '(2.4 GHz) (Authentication State: %s)'
-                  % assoc.AssociatedDeviceAuthenticationState)
-
-          else:
-            self.data['ssid5'] = wlconf.SSID
-            if wlconf.WPAAuthenticationMode == 'PSKAuthentication':
-              wpa['5 GHz'] = '(configured)'
-            wlan[wlconf.BSSID] = '(5 GHz) (%s)' % wlconf.Status
-            for k, assoc in wlconf.AssociatedDeviceList.iteritems():
-              devices[assoc.AssociatedDeviceMACAddress] = (
-                  '(5 GHz) (Authentication State: %s)'
-                  % assoc.AssociatedDeviceAuthenticationState)
-
-      self.data['wirelesslan'] = wlan
-      self.data['wirelessdevices'] = devices
-      self.data['wpa2'] = wpa
-
-      if self.data['ssid24']:
-        if self.data['ssid5'] == self.data['ssid24']:
-          self.data['ssid5'] = '(same)'
-
-      try:
-        self.data['upnp'] = self.root.UPnP.Device.Enable
-      except AttributeError:
-        self.data['upnp'] = 'Off'
-
-      try:
-        dns = self.root.DNS.SD.ServiceList
-        for i, serv in dns.iteritems():
-          self.data['dyndns'] = serv.InstanceName
-          self.data['domain'] = serv.Domain
-      except AttributeError:
-        pass
-
-      # TODO: Verify if these are the correct values.
-      try:
-        wan = self.root.InternetGatewayDevice.WANDeviceList
-      except AttributeError:
-        pass
-      else:
-        for i, dev in wan.iteritems():
-          for j, conn in dev.WANConnectionDeviceList.iteritems():
-            for k, ipconn in conn.WANIPConnectionList.iteritems():
-              self.data['wanmac'] = ipconn.MACAddress
-              self.data['wanip'] = ipconn.ExternalIPAddress
-
+  def UpdateCheckSum(self):
     newchecksum = hashlib.sha1(unicode(
         sorted(list(self.data.items()))).encode('utf-8')).hexdigest()
     self.data['checksum'] = newchecksum
 
+
+  def UpdateLatestDict(self):
+    """Updates the dictionary and checksum value."""
+
+    if not self.root:
+      return
+
+    self.data = {}
+    self.data['subnetmask'] = ''
+
+    deviceinfo = self.root.Device.DeviceInfo
+    tempstatus = deviceinfo.TemperatureStatus
+    landevlist = self.root.InternetGatewayDevice.LANDeviceList
+    etherlist = self.root.Device.Ethernet.InterfaceList
+
+    if self.cpemach.last_success_response:
+      self.data['acs'] = 'OK (%s)' % self.cpemach.last_success_response
+    else:
+      self.data['acs'] = 'Never Contacted'
+    self.data['softversion'] = deviceinfo.SoftwareVersion
+    self.data['uptime'] = deviceinfo.UpTime
+    self.data['username'] = self.root.Device.ManagementServer.Username
+
+    t = dict()
+    try:
+      for i, sensor in tempstatus.TemperatureSensorList.iteritems():
+        t[sensor.Name] = sensor.Value
+      self.data['temperature'] = t
+    except AttributeError:
+      pass
+
+    t = dict()
+    for i, inter in self.root.Device.IP.InterfaceList.iteritems():
+      for j, ip4 in inter.IPv4AddressList.iteritems():
+        t[ip4.IPAddress] = '(%s)' % ip4.Status
+        self.data['subnetmask'] = ip4.SubnetMask
+      for j, ip6 in inter.IPv6AddressList.iteritems():
+        if ip6.IPAddress[:4] != 'fe80':
+          t[ip6.IPAddress] = '(%s)' % ip6.Status
+    self.data['lanip'] = t
+
+    t = dict()
+    for i, interface in etherlist.iteritems():
+      t[interface.MACAddress] = '(%s)' % interface.Status
+    self.data['wiredlan'] = t
+
+    t = dict()
+    for i, inter in self.root.Device.MoCA.InterfaceList.iteritems():
+      for j, dev in inter.AssociatedDeviceList.iteritems():
+        t[dev.NodeID] = dev.MACAddress
+    self.data['wireddevices'] = t
+
+    wlan = dict()
+    devices = dict()
+    wpa = dict()
+    self.data['ssid5'] = ''
+
+    for i, dev in landevlist.iteritems():
+      for j, wlconf in dev.WLANConfigurationList.iteritems():
+        if wlconf.Channel in range(1, 12):
+          self.data['ssid24'] = wlconf.SSID
+          if wlconf.WPAAuthenticationMode == 'PSKAuthentication':
+            wpa['2.4 GHz'] = '(configured)'
+          wlan[wlconf.BSSID] = '(2.4 GHz) (%s)' % wlconf.Status
+          for k, assoc in wlconf.AssociatedDeviceList.iteritems():
+            devices[assoc.AssociatedDeviceMACAddress] = (
+                '(2.4 GHz) (Authentication State: %s)'
+                % assoc.AssociatedDeviceAuthenticationState)
+
+        else:
+          self.data['ssid5'] = wlconf.SSID
+          if wlconf.WPAAuthenticationMode == 'PSKAuthentication':
+            wpa['5 GHz'] = '(configured)'
+          wlan[wlconf.BSSID] = '(5 GHz) (%s)' % wlconf.Status
+          for k, assoc in wlconf.AssociatedDeviceList.iteritems():
+            devices[assoc.AssociatedDeviceMACAddress] = (
+                '(5 GHz) (Authentication State: %s)'
+                % assoc.AssociatedDeviceAuthenticationState)
+
+    self.data['wirelesslan'] = wlan
+    self.data['wirelessdevices'] = devices
+    self.data['wpa2'] = wpa
+
+    if 'ssid24' in self.data and 'ssid5' in self.data:
+      if self.data['ssid5'] == self.data['ssid24']:
+        self.data['ssid5'] = '(same)'
+
+    try:
+      self.data['upnp'] = self.root.UPnP.Device.Enable
+    except AttributeError:
+      self.data['upnp'] = 'Off'
+
+    try:
+      dns = self.root.DNS.SD.ServiceList
+      for i, serv in dns.iteritems():
+        self.data['dyndns'] = serv.InstanceName
+        self.data['domain'] = serv.Domain
+    except AttributeError:
+      pass
+
+    # TODO: Verify if these are the correct values.
+    try:
+      wan = self.root.InternetGatewayDevice.WANDeviceList
+    except AttributeError:
+      pass
+    else:
+      for i, dev in wan.iteritems():
+        for j, conn in dev.WANConnectionDeviceList.iteritems():
+          for k, ipconn in conn.WANIPConnectionList.iteritems():
+            self.data['wanmac'] = ipconn.MACAddress
+            self.data['wanip'] = ipconn.ExternalIPAddress
+    self.UpdateCheckSum()
