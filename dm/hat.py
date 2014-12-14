2a8e86542b7c27051fb0fdb663627a1a838116e8
Divyesh Shah <dpshah@google.com>
2014-12-13 16:12:40 -0800

    catawampus: ads: Add disk_space_cleanup_interval_secs setting
    Change-Id: I2c5b595c1ff475e9072567c592346538dde737ae
    Google-Bug-Id: 18281805

--- dm/hat.py
+++ dm/hat.py
@@ -53,6 +53,7 @@ class Hat(CATABASE.HAT):
   DiskSpaceLimitGb = tr.cwmptypes.TriggerUnsigned()
   DiskSpaceLowWatermarkPercent = tr.cwmptypes.TriggerUnsigned()
   DiskSpaceHighWatermarkPercent = tr.cwmptypes.TriggerUnsigned()
+  DiskSpaceCleanupIntervalSecs = tr.cwmptypes.TriggerUnsigned()
   HatCatalogPollingIntervalSecs = tr.cwmptypes.TriggerUnsigned()
 
   GFTSUrl = tr.cwmptypes.TriggerString()
@@ -105,6 +106,8 @@ class Hat(CATABASE.HAT):
                               'disk_space_low_watermark_percent')
       self.printIfSetUnsigned(f, self.DiskSpaceHighWatermarkPercent,
                               'disk_space_high_watermark_percent')
+      self.printIfSetUnsigned(f, self.DiskSpaceCleanupIntervalSecs,
+                              'disk_space_cleanup_interval_secs')
       self.printIfSetUnsigned(f, self.HatCatalogPollingIntervalSecs,
                               'hat_catalog_polling_interval_secs')
       self.printIfSetString(f, self.GFTSUrl, 'gfts_url')
